53e642a1965e647cb6547e5cbd52cc35ccccc0c6
yang0925 <keun0602@naver.com>
2020-10-29 12:32:06 +0900

    [Add] add QuizQuestionsActivity

---
+++ app/src/main/java/com/example/quizapp/Constants.kt
@@ -0,0 +1,129 @@
+package com.example.quizapp
+
+object Constants {
+    fun getQuestions(): ArrayList<Question> {
+        val questionList = ArrayList<Question>()
+
+        val que1 = Question(
+            1,
+            "What country does this flag belong to?",
+            R.drawable.ic_flag_of_argentina,
+            "Argentina",
+            "Australia",
+            "Armenia",
+            "Austria",
+            1
+        )
+        questionList.add(que1)
+
+        val que2 = Question(
+            2,
+            "What country does this flag belong to?",
+            R.drawable.ic_flag_of_denmark,
+            "Dominica",
+            "New Zealand",
+            "Denmark",
+            "Austria",
+            3
+        )
+        questionList.add(que2)
+
+        val que3 = Question(
+            3,
+            "What country does this flag belong to?",
+            R.drawable.ic_flag_of_france,
+            "Belgium",
+            "France",
+            "Fiji",
+            "Kuwait",
+            2
+        )
+        questionList.add(que3)
+
+        val que4 = Question(
+            4,
+            "What country does this flag belong to?",
+            R.drawable.ic_flag_of_germany,
+            "Palestine",
+            "Gerogia",
+            "Denmark",
+            "Germany",
+            4
+        )
+        questionList.add(que4)
+
+        val que5 = Question(
+            5,
+            "What country does this flag belong to?",
+            R.drawable.ic_flag_of_philippines,
+            "Argentina",
+            "Philippines",
+            "Tuvalu",
+            "Gabon",
+            2
+        )
+        questionList.add(que5)
+
+        val que6 = Question(
+            6,
+            "What country does this flag belong to?",
+            R.drawable.ic_flag_of_south_korea,
+            "Jordan",
+            "South Korea",
+            "India",
+            "United States",
+            2
+        )
+        questionList.add(que6)
+
+        val que7 = Question(
+            7,
+            "What country does this flag belong to?",
+            R.drawable.ic_flag_of_spain,
+            "Ireland",
+            "Sudan",
+            "Armenia",
+            "Spain",
+            4
+        )
+        questionList.add(que7)
+
+        val que8 = Question(
+            8,
+            "What country does this flag belong to?",
+            R.drawable.ic_flag_of_switzerland,
+            "Switzerland",
+            "Australia",
+            "Denmark",
+            "Ethiopia",
+            1
+        )
+        questionList.add(que8)
+
+        val que9 = Question(
+            9,
+            "What country does this flag belong to?",
+            R.drawable.ic_flag_of_turkey,
+            "China",
+            "Switzerland",
+            "Turkey",
+            "Austria",
+            3
+        )
+        questionList.add(que9)
+
+        val que10 = Question(
+            10,
+            "What country does this flag belong to?",
+            R.drawable.ic_flag_of_united_states,
+            "South Korea",
+            "Greece",
+            "Egypt",
+            "United States",
+            4
+        )
+        questionList.add(que10)
+
+        return questionList
+    }
+}
--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.quizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +13,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+
+        btn_start.setOnClickListener {
+            if(et_name.text.toString().isEmpty()) {
+                Toast.makeText(this, "Please enter your name", Toast.LENGTH_SHORT).show()
+            }else {
+                val intent = Intent(this, QuizQuestionsActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
     }
 }
